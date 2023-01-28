24c16b0db18e627e76c73608dc855eca69d38d03
illika <steve.milan7@gmail.com>
2023-01-27 19:22:14 -0500

    HttpClientModule y HttpClient

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,16 @@ import { AppComponent } from './app.component';
 import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
 
+import { HttpClientModule } from '@angular/common/http';
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
+    
     SharedModule,
     GifsModule,
   ],
--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -1,13 +1,18 @@
 import { Injectable } from '@angular/core';
 
+import { HttpClient } from '@angular/common/http';
+
 @Injectable({
   providedIn: 'root'
 })
 export class GifsService {
 
-  constructor() { }
-
+  private apiKey: string = "vTCPCODGphd08aDaHPUKoN3AMc34mVal";
   private _historial: string[] = [];
+
+  resultados: any[] = [];
+
+  constructor(private http: HttpClient) { }
 
   get historial() {
     return [...this._historial];
@@ -16,5 +21,8 @@ export class GifsService {
   buscarGifs(query: string) {
     query = query.trim().toLocaleLowerCase();
     if (!this._historial.includes(query)) this._historial.unshift(query);
+
+    this.http.get(`https://api.giphy.com/v1/gifs/search?api_key=vTCPCODGphd08aDaHPUKoN3AMc34mVal&q=${query}&limit=10`)
+      .subscribe((response: any) => this.resultados = response.data);
   }
 }
